0fceeb82a163c566bfe9647e6930d9ae9cbc9e2a
cmayekar <cmayekar@alliedtelecom.net>
2018-10-13 20:52:16 -0400

    Views started, only Notification List

--- Discover/Mappy/views.py
+++ Discover/Mappy/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from . import models
+from . import serializers
+
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import AnonymousUser, Permission, User, Group
+from django.http import HttpResponse
+from rest_framework import permissions, generics
+from rest_framework.exceptions import NotAuthenticated
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+class NotificationList(
+	generics.ListCreateAPIView):
+	queryset = models.Notification.objects.all()
+	serializer_class = serializers.NotificationSerializer
